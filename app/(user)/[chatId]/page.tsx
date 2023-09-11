ef8c8046a3b4398bae6cfad4c60f27767a31b8a7
root <root@taylorswift.ghostx>
2023-09-11 00:45:17 -0400

    Hello World Create Chat list

---
+++ app/(user)/[chatId]/page.tsx
@@ -0,0 +1,15 @@
+function ChatPage() {
+  return (
+    <>
+      {/* Admin Controls */}
+      {/* ChatMember Settings */} 
+
+        {/* Chat Messages */ }
+
+        {/* Chat Input */}
+        <ChatList />
+    </>
+  )
+}
+
+export default ChatPage
---
+++ app/(user)/chat/page.tsx
@@ -0,0 +1,18 @@
+type Props = {
+  paramsss: {};
+  searchParams: {
+    error: string;
+  };
+};
+
+function ChatsPage({ searchParams: { error } }: Props) {
+  return (
+    <div>
+      {/* Chat Permission Chat */}
+
+      {/* ChatList */}
+    </div>
+  );
+}
+
+export default ChatsPage;
