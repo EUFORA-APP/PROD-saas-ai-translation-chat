24c0a6396c7674cbf459847cc63e68afb513df2d
Austin Rhoades <x@auroraholding.net>
2023-11-18 18:19:02 -0500

    Ferrari LaFerrari Aperta .2 Million, User UID now logged in Auth for firebase and Db

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import ClientProviders from '../components/ClientProviders';
-
-const inter = Inter({ subsets: ['latin'] })
+import FirebaseAuthProvider from '@/components/FirebaseAuthProvider';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -20,17 +18,20 @@ export default function RootLayout({
   return (
     <ClientProviders>
       <html lang="en">
-        <body className="flex flex-col min-h-screen">        
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >          
-          <Header />
+        <body className="flex flex-col min-h-screen">
+          <FirebaseAuthProvider>
             
-          {children}
-          </ThemeProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >          
+              <Header />
+              
+              {children}
+            </ThemeProvider>
+          </FirebaseAuthProvider>
         </body>
       </html>
     </ClientProviders>
--- components/FirebaseAuthProvider.tsx
+++ components/FirebaseAuthProvider.tsx
@@ -12,16 +12,13 @@ async function syncFirebaseAuth(session: Session) {
       await signInWithCustomToken(auth, session.firebaseToken)
     } catch (error) {
       console.error("Error signing in with custom token", error);
-
     }
+  } else {
+    auth.signOut();
   }
 }
 
-function FirebaseAuthProvider({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
+function FirebaseAuthProvider({ children }: { children: React.ReactNode }) {
     const {data: session} = useSession();
     
     useEffect(() => {
