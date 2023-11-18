3f02dc650321029a69b23a51fefc7e45c358b44e
Austin Rhoades <x@auroraholding.net>
2023-11-18 01:06:29 -0500

    oCtopus deep undervalue Great piCk

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div> Hello World</div>
+      <div>$CGC December 12, 2023 $420 Deep Octo Undervalue Stock</div>
     </main>
   )
 }
