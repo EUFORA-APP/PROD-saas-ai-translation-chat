c4d6808411c833f24650b4ac5f590f67fc5fd516
Austin Rhoades <x@auroraholding.net>
2023-11-18 01:44:51 -0500

    duh

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>$CGC December 12, 2023 USD $1,430.00000000 Deep Octo Undervalue Stock $SHIB $1.00000000 #AmericanMetricSystem</div>
+      <div>$CGC December 12, 2023 USD $OneHundred43Thousand Deep Octo Undervalue Stock $SHIB $1Dollar</div>
     </main>
   )
 }
