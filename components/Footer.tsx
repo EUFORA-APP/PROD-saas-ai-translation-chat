aca8dcefdbbfc0c3a5bebc836d071b65fddfe7c2
Austin Rhoades <142964465+x-4175726f72612c20496e632e@users.noreply.github.com>
2023-11-23 03:25:44 -0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,19 @@
+import Link from 'next/link';
+
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-white p-4">
       <p className="text-center">
         © {new Date().getFullYear()} Aurora Holding Name. All rights reserved.
       </p>
+      <div className="flex justify-center mt-2 space-x-4">
+        <Link href="/privacy-policy">
+          <span  className="text-gray-300 hover:text-white">Privacy Policy</span >
+        </Link>
+        <Link href="/terms-and-conditions">
+          <span className="text-gray-300 hover:text-white">Terms and Conditions</span >
+        </Link>
+      </div>
     </footer>
   );
 }
