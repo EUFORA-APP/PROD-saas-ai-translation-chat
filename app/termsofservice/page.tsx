d1671addd413fdb146bc9f0a628127456eb88785
Austin Rhoades <142964465+x-4175726f72612c20496e632e@users.noreply.github.com>
2023-11-25 06:50:23 -0500

    Update page.tsx

--- app/termsofservice/page.tsx
+++ app/termsofservice/page.tsx
@@ -2,8 +2,138 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
-        <h1>Hello World, Lets Build a Terms of Service</h1>
-    </main>  
-    )
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold text-center text-indigo-600">
+        Privacy Policy: 
+      </h1>
+      <h2 className="font-semibold text-center text-gray-700 mt-4">
+      Connect with peace of mind
+      </h2>
+      <div className="flex justify-center items-center mt-8 text-xl">
+        <img
+          src="/author.jpg"
+          alt="ADD IMAGE"
+          className="w-12 h-12 rounded-full mr-4"
+        />
+        <div className="text-gray-600">
+          <p className="text-sm">Express yourself freely and safely</p>
+          <p className="text-sm">November 25, 2023</p>
+        </div>
+      </div>
+      <div className="mt-12"></div>
+
+      <h3 className="font-medium text-gray-700 mt-8 text-xl">
+      Terms of Service
+    </h3>
+    <p className="text-gray-700 leading-8 mt-4">
+    By signing up for Eufora, you accept our Privacy Policy and agree not to:
+
+Use our service to send spam or scam users.
+Promote violence on publicly viewable Eufora channels, bots, etc.
+Post illegal pornographic content on publicly viewable Eufora channels, bots, etc.
+We reserve the right to update these Terms of Service later.
+
+Citizens of EU countries and the United Kingdom must be at least 16 years old to sign up.
+    </p> 
+    
+    <h3 className="font-medium text-gray-700 mt-8 text-xl">
+      Terms of Service for EUFORA PRO MEMBERSHIP
+    </h3>
+    <h3 className="font-medium text-gray-700 mt-8 text-xl">
+    Introduction
+
+    </h3>
+    <p className="text-gray-700 leading-8 mt-4">
+    Pro, may be available for purchase in certain countries – offering an even more powerful messaging experience and a chance to support the continuing development of the Telegram platform.
+    Telegram Premium is a subscription membership that unlocks exclusive features and improved functionality. By purchasing Telegram Premium, you agree that you are authorizing recurring monthly or yearly payments, which will be made to Telegram using the payment method you selected until the subscription is canceled by you or Telegram.
+
+Telegram uses third-party payment processors to securely facilitate purchases of Telegram Premium.
+    </p>
+    <h3 className="font-medium text-gray-700 mt-8 text-xl">
+    Payment
+    </h3>
+    <p className="text-gray-700 leading-8 mt-4">
+    You can manage your Telegram Premium subscription by accessing the Telegram Premium section in the Settings of your Telegram account. If your payment method is declined, you must provide new payment information or your Telegram Premium subscription will be canceled. If you provide new payment information and are successfully charged, your new subscription period will be based on the original renewal date and not the date of the successful charge.
+    </p>
+    <h3 className="font-medium text-gray-700 mt-8 text-xl">
+    Taxes and Conversion Rates
+    </h3>
+    <p className="text-gray-700 leading-8 mt-4">
+    Users in certain regions may be required to pay local taxes, which will be calculated and added to the total cost of a purchase. Payment providers may apply their own commissions and conversion rates to payments in various currencies. This is outside of Telegram’s control.
+    </p>
+    <h3 className="font-medium text-gray-700 mt-8 text-xl">
+    Cancellation Policy
+    </h3>
+    <p className="text-gray-700 leading-8 mt-4">
+    You can cancel Telegram Premium any time. You can cancel your subscriptions by visiting the Subscriptions section.
+    </p>
+    <h3 className="font-medium text-gray-700 mt-8 text-xl">
+    Refunds and Effective Cancellation
+    </h3>
+    <p className="text-gray-700 leading-8 mt-4">
+    Your subscription will remain active from the time you cancel until the end of your current billing period, and you will not receive a refund or credit for any remaining days in your current billing period. At the end of your subscription you will lose access to all features associated with Telegram Premium.
+
+Deleting your Telegram account or removing the Telegram application from your device does not cancel your subscription.
+    </p>
+    <h3 className="font-medium text-gray-700 mt-8 text-xl">
+    Termination
+    </h3>
+    <p className="text-gray-700 leading-8 mt-4">
+    Failure to comply with the Telegram Terms of Service may result in a temporary or a permanent ban from Telegram or some of its services. In such instances, you might lose the benefits of Telegram Premium and we will not compensate you for this loss.
+
+
+    </p>
+    <h3 className="font-medium text-gray-700 mt-8 text-xl">
+    Telegram can decide to stop offering subscriptions at any time, including in response to unforeseen circumstances beyond our control, or to comply with a legal requirement; in this case we will cancel your subscription and refund the prorated portion of any prepaid subscription fee equal to the remaining unused term of the subscription.
+      
+    </h3>
+    <p className="text-gray-700 leading-8 mt-4">
+    You are responsible for all applicable taxes, data plans, internet fees, and other fees associated with your use of Telegram.
+
+    Generally, all charges for purchases are nonrefundable. It is not possible to get a “partial refund” or credit if a user wishes to cancel their recurring subscription early – it will remain valid until the end of its purchased duration.
+
+
+    </p>
+    <h3 className="font-medium text-gray-700 mt-8 text-xl">
+    Changes
+    </h3>
+    <p className="text-gray-700 leading-8 mt-4">
+    While we do not anticipate frequent changes to the price of Telegram Premium, we reserve the right to change both these terms and the fees in effect on renewal of your subscription, to reflect factors such as changes to our business, different product offerings, or new economic conditions.
+
+
+    </p>
+    <h3 className="font-medium text-gray-700 mt-8 text-xl">
+    Advance Notice
+    </h3>
+    <p className="text-gray-700 leading-8 mt-4">
+    We will give you no less than 30 days' advance notice of any material changes via a message to your Telegram account and you will have the opportunity to cancel your subscription before the new fee comes into effect. Any correspondence from the Telegram Team about a user’s Premium account will be sent via verified service accounts in the Telegram app.
+
+
+    </p>
+    <h3 className="font-medium text-gray-700 mt-8 text-xl">
+    The Scope of Premium Services
+    </h3>
+    <p className="text-gray-700 leading-8 mt-4">
+    The services included in the Telegram Premium subscription (“Premium Services”) are listed in the Premium Section. From time to time, we may add new items into the Premium Services. We may also remove some of the existing Premium Services if we are no longer able to provide them for legal or technical reasons. In addition, some Premium Services may become free of charge for all Telegram users in the future.
+    </p>
+    <h3 className="font-medium text-gray-700 mt-8 text-xl">
+    Availability
+    </h3>
+    <p className="text-gray-700 leading-8 mt-4">
+    Telegram strives to make its services available for all users all the time, but occasional technical issues may occur, temporarily preventing us from delivering services, including Premium Services, to our users. Telegram will not compensate you for any direct or indirect loss resulting from our temporary inability to provide any free or paid services.
+
+
+    </p>
+    <h3 className="font-medium text-gray-700 mt-8 text-xl">
+    Software Requirements
+    </h3>
+    <p className="text-gray-700 leading-8 mt-4">
+    Some Premium Services may require the latest version of the Telegram application to be installed on your device. In some cases, users you interact with may also need to have the latest version of the Telegram application installed on their devices in order to be able to view the content you share with them by the use of Premium Services. For example, exclusive stickers available only to the Telegram Premium subscribers or documents larger than 2 GB may be correctly viewed only by users with an up-to-date version of Telegram.
+
+    In general, users you interact with will not have to purchase a Telegram Premium subscription in order to view the content you share with them by the use of Premium Services.
+
+
+    </p>         
+      </div>
+  );
 }
