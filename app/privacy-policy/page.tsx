fbf227e280f8616fb1070662d11877af998611e1
Austin Rhoades <142964465+x-4175726f72612c20496e632e@users.noreply.github.com>
2023-11-25 06:26:41 -0500

    Update page.tsx

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,9 +1,153 @@
-import Image from 'next/image'
+import React from "react";
 
-export default function Home() {
+export default function Page() {
   return (
-    <main>
-        <h1>Hello World, Let's build a Privacy-Policy</h1>
-    </main>  
-    )
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold text-center text-indigo-600">
+        Privacy Policy: 
+      </h1>
+      <h2 className="font-semibold text-center text-gray-700 mt-4">
+      Connect with peace of mind
+      </h2>
+      <div className="flex justify-center items-center mt-8">
+        <img
+          src="/author.jpg"
+          alt="ADD IMAGE"
+          className="w-12 h-12 rounded-full mr-4"
+        />
+        <div className="text-gray-600">
+          <p className="text-sm">Express yourself freely and safely</p>
+          <p className="text-sm">November 25, 2023</p>
+        </div>
+      </div>
+      <div className="mt-12">
+        
+      <h3 className="font-medium text-gray-700 mt-8">
+          1. Introduction
+        </h3>
+        <p className="text-gray-700 leading-8"> We care about your privacy and we want you to feel safe and comfortable when you use our cloud-based messaging services (the “Services”). That’s why we, EUFORA Messenger Inc. (“EUFORA”), have created this Privacy Policy to explain how we collect, use, and protect your personal data that you share with us, or that we get from other sources, when you use our Services. In this Privacy Policy, ‘we’, ‘us’ and ‘our’ refers to EUFORA, and ‘you’ refers to you, the user of the Services.
+        </p>
+        <h3 className="font-medium text-gray-700 mt-8">
+          1.1 Privacy Principles
+        </h3>
+        <p className="text-gray-700 leading-8 mt-4">
+        EUFORA.app has two fundamental principles when it comes to collecting and processing private data:
+
+        1. We don&apos;t use your data to show you ads.
+        2. We only store the data that EUFORA.app needs to function as a secure and feature-rich messaging service..
+        </p>
+
+        
+        <h3 className="font-medium text-gray-700 mt-8">
+          1.2 Terms of Service
+        </h3>
+        <p className="text-gray-700 leading-8 mt-4">
+        This Privacy Policy forms part of our Terms of Service, which describes the terms under which you use our Services and which are available at {" "}
+          <a
+            href="https://EUFORA.app/termsofservice."
+            className="text-indigo-600 underline"
+          >
+            this link. 
+          </a>
+          </p>
+        <p className="text-gray-700 leading-8 mt-4">
+          This Privacy Policy should therefore be read in conjunction with those terms.</p>
+        <p className="text-gray-700 leading-8 mt-4">
+        If you live in a country in the European Economic Area (EEA), the Services are provided by EUFORA, which for the purposes of applicable data protection legislation is the data controller responsible for your personal data when you use our Services. However, as EUFORA is located outside the EEA, we designate a third party, European Data Protection Office (EDPO), as our representative pursuant to Article 27 of the GDPR. You can direct to EDPO any of your GDPR-related queries:
+
+        by using EDPO’s online request form at https://edpo.com or
+        by writing to EDPO at Avenue Huart Hamoir 71, 1030 Brussels, Belgium.
+          </p>
+
+          <h3 className="font-medium text-gray-700 mt-8">
+        2. Legal Ground for Processing Your Personal Data
+        </h3>
+        <p className="text-gray-700 leading-8 mt-4">
+        We process your personal data on the ground that such processing is necessary to further our legitimate interests (including: (1) providing effective and innovative Services to our users; and (2) to detect, prevent or otherwise address fraud or security issues in respect of our provision of Services), unless those interests are overridden by your interest or fundamental rights and freedoms that require protections of personal data.
+          </p>
+        
+        <h3 className="font-medium text-gray-700 mt-8">
+        3. Data
+        </h3>
+        <p className="text-gray-700 leading-8 mt-4">
+        EUFORA&apos;s is a communication service. You provide basic account data to create a EUFORA&apos;s account.
+          </p>
+        <p className="text-gray-700 leading-8 mt-4">
+        To make it easier for your contacts and other people to reach you and recognize who you are, the screen name you choose, your profile pictures, and your name (should you choose to set one) on EUFORA&apos;s are connected to your Google Account. 
+          </p>
+        <p className="text-gray-700 leading-8 mt-4">
+        We do not require your screen name to be your real name.
+          </p>
+        <h3 className="font-medium text-gray-700 mt-8">
+        3.2. Your Email Address
+      </h3>
+        <p className="text-gray-700 leading-8 mt-4">
+        Currently you can only sign in with a Google Gmail account.
+          </p>
+        <h3 className="font-medium text-gray-700 mt-8">
+        3.3. Your Cookies
+              </h3>
+        <p className="text-gray-700 leading-8 mt-4">
+        The only cookies we use are those to operate and provide our Services on the web. The cookies we use are small text files that allow us to provide and customize our Services, and in doing so provide you with an enhanced user experience. Your browser should allow you to control these cookies, including whether or not to accept them and how to remove them. You may choose to block cookies with your web browser, however, if you do disable these cookies you may not be able to log in to EUFORA Web.
+          </p>
+        <h3 className="font-medium text-gray-700 mt-8">
+        3.4 Your Chats
+      </h3>
+        <p className="text-gray-700 leading-8 mt-4">
+          Your Chats&apos; are stored securely in the cloud. We are a software as a service(SAAS). We store messages from your chats on our servers so that you can access your data from any of your devices anytime and almost anywhere without having to rely on backups. All data is stored and secure. You are always able to delete the chats which you have created.
+          </p>
+        <p className="text-gray-700 leading-8 mt-4">
+        Retention. Unless stated otherwise in this Privacy Policy, the personal data that you provide us will only be stored for as long as it is necessary for us to fulfill our obligations in respect of the provision of the Services.
+          </p>
+        <h3 className="font-medium text-gray-700 mt-8">
+        4. Processing Your Personal Data
+        </h3>
+        <p className="text-gray-700 leading-8 mt-4">
+        We will process your data to deliver your cloud chat history, including messages, media and files, to any devices of your choosing without a need for you to use third-party backups or cloud storage. 
+          </p>
+        
+        <h3 className="font-medium text-gray-700 mt-8">
+        5. Third Party Payment Services
+        </h3>
+        <p className="text-gray-700 leading-8 mt-4">
+        EUFORA does not process payments from users and instead relies on different payment providers around the world. It is the payment providers that handle and store your credit card details. Neither EUFORA nor the merchants on the platform (bot developers) have access to this information. Although we work with payment providers they are completely independent from EUFORA. Please study their relevant privacy policies before making your data available to them.
+          </p>
+        <p className="text-gray-700 leading-8 mt-4">
+        When making a purchase, you enter your credit card details into a form supplied by the payment provider that will be processing the payment, and this information goes directly to the payment provider&apos;s server. Your credit card information never reaches EUFORA&apos;s servers. We do not access and do not store your credit card information. When you save your credit card info, it is saved on the respective payment provider&apos;s servers and the payment provider gives EUFORA a token that you can reuse for future payments. It is not possible to reconstruct your credit card info from the token.
+          </p>
+        <p className="text-gray-700 leading-8 mt-4">
+        When you enter shipping information in the process of placing an order, we send it directly to the merchant developer.
+          </p>
+        <p className="text-gray-700 leading-8 mt-4">
+        Due to the fact that EUFORA doesn&apos;t store any credit card details or transaction information, it is impossible for us to handle complaints or cashbacks – any disputed payments are the responsibility of the bot developers, payment providers, and banks that participated in the exchange.
+          </p>
+        
+        <h3 className="font-medium text-gray-700 mt-8">
+        6. Who Your Personal Data May Be Shared With
+        </h3>
+        <p className="text-gray-700 leading-8 mt-4">
+        Other EUFORA Users. Law Enforcement. Third-party partners.
+          </p>
+        
+        <h3 className="font-medium text-gray-700 mt-8">
+        7. Your Rights Regarding the Personal Data You Provide to Us
+        </h3>
+        <p className="text-gray-700 leading-8 mt-4">
+        Other users of our Services with whom you choose to communicate with and share certain information, who may be located outside the EEA. Note that by entering into the Terms of Service and choosing to communicate with such other users of EUFORA, you are instructing us to transfer your personal data, on your behalf, to those users in accordance with this Privacy Policy. We employ all appropriate technical and organizational measures (including encryption of your personal data) to ensure a level of security for your personal data that is appropriate to the risk.
+          </p>
+
+        <p className="text-gray-700 leading-8 mt-4">
+        Translation of Chats, Channels and specific Messages at the Request of the User. Users can choose to translate a specific text message into a different language by tapping the language bar that is set to english as default.
+        </p>
+                
+        <h3 className="font-medium text-gray-700 mt-8">
+        8. Your Rights Regarding the Personal Data You Provide to Us
+        </h3>
+
+          <p className="text-gray-700 leading-8 mt-4">
+          Under applicable data protection legislation, in certain circumstances, you have rights concerning your personal data. You have a right to: (1) request a copy of all your personal data that we store and to transmit that copy to another data controller; (2) delete or amend your personal data; (3) restrict, or object to, the processing of your personal data; (4) correct any inaccurate or incomplete personal data we hold on you; and (5) lodge a complaint with national data protection authorities regarding our processing of your personal data.
+          </p>        
+      </div>
+    </div>
+  );
 }
